3fac704cbbc1f7f7e0d03a18a123f429b696d072
Jaime <176935470+HolaRene@users.noreply.github.com>
2026-03-11 12:38:18 -0600

    Agregar configuración de Babel y crear componentes de onboarding

---
+++ app/onboarding.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const onBoarding = () => {
+    return (
+        <View>
+            <Text>Tablero principal</Text>
+        </View>
+    )
+}
+
+export default onBoarding
+
+const styles = StyleSheet.create({})
